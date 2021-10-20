1c0e970b9031ac35f3dacadb06bc281cde50c492
TK2train <89703225+TK2train@users.noreply.github.com>
2021-10-20 13:54:32 +0530

    Add files via upload

--- finding_highest_peak_with_linear_search.py
+++ finding_highest_peak_with_linear_search.py
@@ -1,17 +1,21 @@
 def find_peak(A):
+	max_peak_value = max_peak_index = -1
 	peak = A[0]
-	for i in range(1, len(A)-2):
+	index = 0
+	for i in range(1, len(A)-1):
 		prev = A[i-1]
 		curr = A[i]
 		next = A[i+1]
 		if curr > prev and curr > next:
 			index = i
 			peak = curr
+		if peak > max_peak_value:
+			max_peak_value, max_peak_index = peak, index
 
-        if len(A)-1 > peak:
-            return A[len(A)-1]
- 
-        return A[index]
+	if A[len(A)-1] > peak:
+		return A[len(A)-1], len(A)-1
 
-A = [35, 5, 20, 2, 40, 25, 80, 25, 15, 40]
+	return max_peak_value, max_peak_index
+
+A = [35, 5, 20, 2, 90, 25, 80, 25, 115, 40]
 print A, "\n", find_peak(A)
--- finding_peak_with_binary_search.py
+++ finding_peak_with_binary_search.py
@@ -2,18 +2,18 @@ def find_peak(A):
     if not A:
         return -1
 
-    l, r = 0, len(A) - 1
-    while l + 1 < r:
-        mid = l + (r - l) / 2
+    left, right = 0, len(A) - 1
+    while left + 1 < right:
+        mid = left + (right - left) / 2
         if A[mid] < A[mid - 1]:
-            r = mid
+            right = mid
         elif A[mid] < A[mid + 1]:
-            l = mid
+            left = mid
         else:
             return mid
-    mid = l if A[l] > A[r] else r
+    mid = left if A[left] > A[right] else right
     return mid
 
 A = [35, 5, 20, 2, 40, 25, 80, 25, 15, 40]
-p = find_peak(A)
-print A, "\n", A[p]
+peak = find_peak(A)
+print A, "\n", A[peak]
